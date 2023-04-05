04697ffb28f3b973690cacd4d4794014402fc22b
Sahil Vora <sahilvora225@gmail.com>
2023-04-05 22:09:37 +0530

    Added fetch data method

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 - The resultant tsv file is stored in ```app/csv_data_store/``` folder
 - The script also logs certain messages in the ```app/logs/``` folder
 - To run the script run the following command in the root directory of the project
+
 ```sudo docker-compose run --rm app sh -c "python3 crawler.py <restaurant_id>"```
 - Please do update the restaurant_id argument in the above command with valid int
--- app/crawler.py
+++ app/crawler.py
@@ -1,5 +1,41 @@
 import sys
+import traceback
 
-print(sys.argv)
+import requests
 
-print('Test script!')
+
+class Crawler():
+    def __init__(self):
+        args = sys.argv
+        try:
+            self.restaurant_id = args[1]
+            self.base_url = 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.56&lng=73.95&restaurantId='
+        except IndexError:
+            raise Exception('Please provide restaurant id as command line argument')
+    
+    def handle(self):
+        data = self.fetch_data()
+        menu = self.get_menu(data)
+        self.write_menu(menu)
+
+    def fetch_data(self):
+        url = self.base_url + self.restaurant_id
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise Exception(f'Unable to fetch data {response.status_code}')
+        
+        response = response.json()
+        return response['data'].get('cards', [])
+    
+    def get_menu(self, data):
+        pass
+
+    def write_menu(self, menu):
+        pass
+
+if __name__ == '__main__':
+    try:
+        crawler = Crawler()
+        crawler.handle()
+    except Exception as e:
+        print(f'ERROR: {e}')
